50002ec7a3b0d7ba368dcc4ffd9228e322aa0459
Katsudon27 <katsudon1127@gmail.com>
2026-05-22 12:24:39 +1000

    Completed TDD for #update_range

--- spec/14_find_number_spec.rb
+++ spec/14_find_number_spec.rb
@@ -217,20 +217,28 @@ describe FindNumber do
     context 'when the guess is less than the answer' do
       subject(:low_guess_game) { described_class.new(0, 9, number_range, 4) }
 
-      xit 'updates min to 5' do
+      it 'updates min to 5' do
+        low_guess_game.update_range
+        expect(low_guess_game.min).to eq(5)
       end
 
-      xit 'does not update max' do
+      it 'does not update max' do
+        low_guess_game.update_range
+        expect(low_guess_game.max).to eq(9)
       end
     end
 
     context 'when the guess is more than the answer' do
       subject(:high_guess_game) { described_class.new(0, 9, number_range, 9) }
 
-      xit 'does not update min' do
+      it 'does not update min' do
+        high_guess_game.update_range
+        expect(high_guess_game.min).to eq(0)
       end
 
-      xit 'updates max to 8' do
+      it 'updates max to 8' do
+        high_guess_game.update_range
+        expect(high_guess_game.max).to eq(8)
       end
     end
 
@@ -246,10 +254,18 @@ describe FindNumber do
     # Write a test for any 'edge cases' that you can think of, for example:
 
     context 'when the guess is 7, min=5, and max=8' do
-      xit 'updates min to the same value as max' do
+      subject(:guess_avg_game) { described_class.new(5, 8, number_range, 7) }
+
+      before do
+        guess_avg_game.update_range
       end
 
-      xit 'does not update max' do
+      it 'updates min to the same value as max' do
+        expect(guess_avg_game.min).to eq(guess_avg_game.max)
+      end
+
+      it 'does not update max' do
+        expect(guess_avg_game.min).to eq(8)
       end
     end
   end
